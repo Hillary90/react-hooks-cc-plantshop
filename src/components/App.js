6563b3afcaa792c16d21b6369c2d785a23048d81
Hillary Tanui <hillarytanui80@gmail.com>
2025-10-30 15:45:37 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+
+  // Fetch initial plant data
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  // Add new plant from form
+  function handleAddPlant(newPlant) {
+    setPlants((prev) => [...prev, newPlant]);
+  }
+
+  // Toggle stock status
+  function handleToggleStock(id) {
+    setPlants((prev) =>
+      prev.map((plant) =>
+        plant.id === id ? { ...plant, inStock: !plant.inStock } : plant
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage
+        plants={plants}
+        onAddPlant={handleAddPlant}
+        onToggleStock={handleToggleStock}
+      />
     </div>
   );
 }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, onAddPlant, onToggleStock }) {
+  const [search, setSearch] = useState("");
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={onAddPlant} />
+      <Search search={search} onSearchChange={setSearch} />
+      <PlantList plants={filteredPlants} onToggleStock={onToggleStock} />
     </main>
   );
 }
